6d85b425dfad4954b13c893b7a724ec1a086f528
Eduardo Serafim <eduardoserafiim05@gmail.com>
2025-01-14 17:09:03 -0400

    adicionei pasta php
    primeira vez usando php, já deixei no git pae

---
+++ Pasta de Exercícios Rafael/phpINSANO/primeiravez/index.php
@@ -0,0 +1,30 @@
+<?php
+
+$fechadura = "Amostadinho";
+
+echo $fechadura;
+
+#-------------------------------------
+
+$listasexy = [1, 2, 3, 4];
+
+var_dump($listasexy);
+
+#-------------------------------------
+
+echo "1" + "1";
+echo "1" . "1";
+
+#-------------------------------------
+
+$pessoa = ["nome" => "Eduardo", "idade" => 16];
+var_dump($pessoa);
+
+#-------------------------------------
+
+echo "Meu nome é {$pessoa["nome"]} e minha idade é {$pessoa["idade"]}"
+
+#-------------------------------------
+
+
+?>
