5382106b087f413defe685496239610799792b27
Eduardo Serafim <eduardoserafiim05@gmail.com>
2025-01-16 22:02:56 -0400

    ex03
    terminei o ex03 de php tmj

--- Pasta de Exercícios Rafael/phpINSANO/exercicios/ex03/index.php
+++ Pasta de Exercícios Rafael/phpINSANO/exercicios/ex03/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Carro Foda Vrum</title>
     <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
@@ -13,17 +13,42 @@
 
 <?php
 
+$criarCarro = new Carro();
+$criarCarro->marca = 'Wolkswagen';
+$criarCarro->modelo = 'Golf GTI';
+$criarCarro->ano = 2023;
+$criarCarro->cor = 'vermelho';
+$criarCarro->exibirDetalhes();
+$criarCarro->alterarCor('preto');
+
+
 class Carro{
     public $marca;
     public $modelo;
     public $ano;
     public $cor;
     public function exibirDetalhes(){
-        echo "<div id='agua'"
+        echo "
+        <div id='agua'>
+            <p>
+                Opa blz, então, ó, pega a visão, você tem um {$this->marca}
+                do modelo {$this->modelo} ano {$this->ano} e a cor dele é {$this->cor}.    
+            </p>
+        </div>";
     }
-}
-
-$criarCarro = new Carro();
+    public function alterarCor($novaCor){
+        $this->cor = $novaCor;
+        echo "
+        <div id='agua'>
+            <p>
+                Opa blz, então, ó, pega a visão, você tem um {$this->marca}
+                do modelo {$this->modelo} ano {$this->ano} e a cor pq eu 
+                dei uma PINTADA NELE kkk slk ri muito quando eu tava
+                fazendo esse exercicio tmj professor meu amigo, a cor ai {$this->cor}.    
+            </p>
+        </div>";
+    }
+};
 
 ?>
 
